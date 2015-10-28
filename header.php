8b1c1ffb06c33b3dad13bd0e2911a3050c09a664
simplon-davidt <david@tarczewski.org>
2015-10-28 09:55:25 +0100

    mise à jour en php

---
+++ header.php
@@ -0,0 +1,3 @@
+<img src="http://lien-vers-image.png" alt="davidt's logo" title="mon logo" />
+<h1>David TARCZEWSKI's resume, Web Developer, Graphist, with Open Source</h1>
+<h2 id="simplon-entete">En formation avec <a href="#simplon">Simplon.co</a>, futur développeur web/mobile pour les associations et leurs outils collaboratifs</h2>
