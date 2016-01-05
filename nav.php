4f15ddb8f7bdfa0a71d29472e1b6812dd0593287
simplon-davidt <dtarcz@gmail.com>
2016-01-05 11:23:08 +0100

    remise du CV en .html pour visibilité sur github et 1ère amélioration du CSS

---
+++ nav.php
@@ -0,0 +1,6 @@
+<ul>
+  <li><a href="#contact-form">Contact</a></li>
+  <li><a href="http://dtar.fr/photos">Portfolio</a></li>
+  <li><a href="https://github.com/simplon-davidt">Mon Github</a></li>
+  <li></li>
+</ul>
---
+++ script/script.js
@@ -0,0 +1,8 @@
+function envoyer() {
+var prenom2 = document.getElementById("prenom").value;
+var nom2 = document.getElementById("nom").value;
+var email2 = document.getElementById("email").value;
+var raison2 = document.getElementById("raison").value;
+var besoin2 = document.getElementById("besoin").value;
+alert("Les données envoyées sont: "+prenom2+", "+nom2+", "+email2+", "+raison2+", "+besoin2)
+};
